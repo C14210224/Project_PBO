e410440af1359caebe89f436931073cbff04811a
TimothyAxel <54768930+TimothyAxel@users.noreply.github.com>
2022-06-22 20:28:40 +0700

    Removed unused classes from PickUp and deleted Enemy

--- Projek_PBO/Projek PBO/core/src/com/projekpbo/game/entities/PickUp.java
+++ Projek_PBO/Projek PBO/core/src/com/projekpbo/game/entities/PickUp.java
@@ -1,42 +1,8 @@
 package com.projekpbo.game.entities;
 
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.TimeUtils;
-
 public interface PickUp {
     void pickedUp(Player player);
     void doEffect();
     void reverseEffect();
     boolean durationExpired();
-}
-
-class PiercingBulletsPickup extends Rectangle implements PickUp {
-    long effectDuration = 10000;
-    long startTime;
-
-    PiercingBulletsPickup() {
-        startTime = TimeUtils.millis();
-    }
-
-    @Override
-    public void pickedUp(Player player) {
-
-    }
-    @Override
-    public void doEffect() {
-
-    }
-
-    @Override
-    public void reverseEffect() {
-
-    }
-
-    @Override
-    public boolean durationExpired() {
-        if(TimeUtils.millis() - startTime > effectDuration) {
-            return true;
-        }
-        return false;
-    }
 }
